1028de406337884f68a0025766319a808429b6f3
Christina Jørgensen <christina@Christinas-MacBook-Pro.local>
2020-03-24 09:49:17 +0100

    css og javascript tilføjet

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded", start);
+
+function start() {
+  //   document.querySelector(".knap1").addEventListener("click", clickedFirst);
+  //   document.querySelector(".knap2").addEventListener("click", clickedSecond);
+  //   document.querySelector(".knap3").addEventListener("click", clickedThird);
+  //   document.querySelector(".knap4").addEventListener("click", clickedFour);
+}
+
+function clickedFirst() {
+  document.querySelector("");
+}
