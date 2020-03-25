77f523dc46c36ddbc1f928eef77ce03793fe2e64
Kirstine Aggergaard <kirstineaggergaard@hotmail.com>
2020-03-25 09:49:11 +0100

    json fil lavet

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 "require(@babel/polyfill)";
 
-window.addEventListener("DOMContentLoaded", start);
+// window.addEventListener("DOMContentLoaded", start);
 
-function start() {
-  console.log("running");
-  getJson();
-}
+// function start() {
+//   console.log("running");
+//   getJson();
+// }
 
-async function getJson() {
-  console.log("JSON");
-  let response = await fetch("static/solen.svg");
-  let mySvgData = await response.text();
-  //   document.querySelector("#lampe").innerHTML = mySvgData;
+// async function getJson() {
+//   console.log("JSON");
+//   let response = await fetch("static/solen.svg");
+//   let mySvgData = await response.text();
+//   //   document.querySelector("#lampe").innerHTML = mySvgData;
+// }
+
+fetch("data.json")
+  .then(result => result.json())
+  .then(data => displayItem(data));
+
+function displayItem(data) {
+  console.log("HElloooo");
+  console.log(data);
 }
