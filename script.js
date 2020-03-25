2203ffbdeb6f20af66459d3073838f9e4c2297f9
Kirstine Aggergaard <kirstineaggergaard@hotmail.com>
2020-03-25 12:11:52 +0100

    kirstines changes

--- script.js
+++ script.js
@@ -7,20 +7,6 @@ function start() {
   getJson();
 }
 
-<<<<<<< HEAD
-fetch("data.json")
-  .then(result => result.json())
-  .then(data => {
-    document.querySelector(".stearin").innerHTML = "<img src='" + data.data[2].img + "'>";
-    console.log(data);
-  });
-
-// function displayItem(data) {
-//   console.log("HElloooo");
-//   console.log(data);
-// ;
-// }
-=======
 async function getJson() {
   console.log("JSON");
   let response1 = await fetch("solen.svg");
@@ -31,4 +17,3 @@ async function getJson() {
   let mySvgData2 = await response2.text();
   document.querySelector("#stearin").innerHTML = mySvgData2;
 }
->>>>>>> 0bce49bcd77ec2454ad4619f973c6885fc7fd9a0
